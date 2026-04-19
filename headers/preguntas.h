d4585958bdc70a02747160df9bd19ebfb8a956c0
Joaquin Lopez <joa.lopez.astor@gmail.com>
2026-04-19 10:34:09 -0400

    Cambie el archivo preguntas.h con la intencion de usar los prototipos de esta para crar el codigo del frontPreg y para que la vale lo use para el Backend

---
+++ headers/preguntas.h
@@ -0,0 +1,51 @@
+#ifndef PREGUNTAS_H
+#define PREGUNTAS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//Constantes
+#define MAX_VIDAS 3
+#define MAX_NIVELES 5
+#define MAX_OPCIONES 4
+#define MAX_TEXTO 256
+#define PISTAS_NIVEL 2
+#define PREGUNTAS_PARA_SUBIR 5
+
+
+
+typedef struct {
+    char enunciado[MAX_TEXTO]; //Texto que se mostrara en cada pregunta
+    char opciones[MAX_OPCIONES][MAX_TEXTO]; //numero de opciones posibles (Responder, pedir pistas o salir)
+    char respuesta_correcta; //Texto de la respuesta correcta
+    char pista[MAX_TEXTO];
+    int nivel; //nivel al que pertenece la pregunta
+    int estado; //para saber si esta pregunta fue usada (1) o no (0)
+} Pregunta;
+
+typedef struct {
+    int vidasAcual;
+    int nivelActual;
+    int pistasRes; //numero de pistas que puedes pedir
+    int puntaje; //Cuantas respuestas correctas hay en un nivel
+}Jugador;
+
+//Prototipos para el enginePreg
+void limpiar_pantalla (); //Limplia la pantalla de la terminal para cada pregunta
+
+void mostrar_encabezado (Jugador* j); //Muestra stats (vidas y pistas)
+
+void mostrar_pregunta (Pregunta* p); //Muestra la pregunta (BRUH)
+
+char obtener_respuesta (); //Captura y filtra el teclado
+
+void mostrar_feadback (int esCorrecto, char respuesta_real); //Reaccion visual a la respuesta
+
+void pantalla_transmicion (int tipo , int nivelactual); //Pantalla de si pasaste al siguiente nivel o perdiste
+
+//TODO: Prototipos del backend de las preguntas 
+
+#endif 
